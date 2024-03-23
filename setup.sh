27bbe8041bf2a03e3bf9870ebd31e142ff5cd660
Rahul <109901628+RahulTarafder@users.noreply.github.com>
2024-03-23 10:41:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,12 +19,12 @@ echo -e "\e[1;34m[*] \e[32mInstalling Packages....\e[0m";
                                           
                                                                                                 
 apt update && apt upgrade -y
-pkg install ruby python python2 -y
-pkg install toilet -y
-pkg install openjdk-17 -y
+apt install ruby python python2 -y
+apt install toilet -y
+apt install openjdk-17 -y
 pip3 install gem
 gem install lolcat
-pkg install wget curl -y
+apt install wget curl -y
 chmod +x setup.sh
 chmod +x apktool.sh
 cd ~/Apktool-termux/files
